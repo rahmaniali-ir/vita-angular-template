459b29cfa9377afb55966a065dad7626c28239cf
Ali Rahmani <alirahmani.prog@gmail.com>
2024-01-27 22:03:43 +0330

    home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  title = 'vida';
+  socialLinks = [
+    {
+      name: 'Facebook',
+      icon: 'ico-facebook',
+      link: '#',
+    },
+    {
+      name: 'Instagram',
+      icon: 'ico-instagram',
+      link: '#',
+    },
+    {
+      name: 'Twitter',
+      icon: 'ico-twitter',
+      link: '#',
+    },
+    {
+      name: 'Telegram',
+      icon: 'ico-telegram',
+      link: '#',
+    },
+  ];
+
+  trackByName(index: number, { name }: { name: string }) {
+    return name;
+  }
 }
